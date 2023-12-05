f95132cdc265fba9010e250261558cf03d2edcb2
Gasan <gasanaff@gmail.com>
2023-12-05 18:15:48 +0300

    Add style.css

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,7 @@ $( document ).ready(function() {
     // create function create table in div#server_data and insert data to table
     function createTable(data) {
         // console.log(data);
-        let table_html = '<table class="table table-striped table-bordered table-hover"><thead><tr><th>servers.id</th><th>ip</th><th>owners.name</th></tr></thead><tbody>';
+        let table_html = '<table class="table"><thead><tr><th>servers.id</th><th>ip</th><th>owners.name</th></tr></thead><tbody>';
         $.each(data, function (key, value) {
             let ip = value.ip == null ? '—' : value.ip;
             table_html += '<tr><td>' + value.server_id + '</td><td>' + ip + '</td><td>' + value.owner_name; + '</td></tr>';
